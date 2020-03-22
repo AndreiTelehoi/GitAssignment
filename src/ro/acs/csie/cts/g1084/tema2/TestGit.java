600f621c4b50e107526688223679401219ef36a9
Andrei Telehoi <andrei.telehoi.99@gmail.com>
2020-03-22 15:53:28 +0200

    Initial commit

---
+++ src/ro/acs/csie/cts/g1084/tema2/TestGit.java
@@ -0,0 +1,11 @@
+package ro.acs.csie.cts.g1084.tema2;
+
+public class TestGit {
+
+    public static final String TITLU_LICENTA = "Solutie informatica pentru managementul filmelor folosind tehnologii web";
+
+    public void main() {
+        System.out.println("Hello Git ! Denumirea proiectului de licență este " + TITLU_LICENTA);
+    }
+    
+}
