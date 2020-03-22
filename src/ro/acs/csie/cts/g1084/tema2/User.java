d7b9599f80600bd479efc5c0aea14095cf947d03
Andrei Telehoi <andrei.telehoi.99@gmail.com>
2020-03-22 16:15:00 +0200

    created User class

---
+++ src/ro/acs/csie/cts/g1084/tema2/User.java
@@ -0,0 +1,15 @@
+package ro.acs.csie.cts.g1084.tema2;
+
+public class User {
+    private String username;
+    private String password;
+    private int nrFilmeVazute;
+
+    public User(String username, String password, int nrFilmeVazute) {
+        super();
+        this.username = username;
+        this.password = password;
+        this.nrFilmeVazute = nrFilmeVazute;
+    }
+    
+}
